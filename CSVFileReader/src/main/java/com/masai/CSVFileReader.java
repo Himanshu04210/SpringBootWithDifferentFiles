218f0ae14369da525a77caed08724055b7cdde20
Himanshu04210 <himanshupoly76075@gmail.com>
2023-10-10 00:10:00 +0530

    some small changes

--- CSVFileReader/src/main/java/com/masai/CSVFileReader.java
+++ CSVFileReader/src/main/java/com/masai/CSVFileReader.java
@@ -11,16 +11,11 @@ public class CSVFileReader {
 	  
 	    try {
 	  
-	        // Create an object of filereader
-	        // class with CSV file as a parameter.
 	        FileReader filereader = new FileReader(file);
 	  
-	        // create csvReader object passing
-	        // file reader as a parameter
 	        CSVReader csvReader = new CSVReader(filereader);
 	        String[] nextRecord;
 	  
-	        // we are going to read data line by line
 	        while ((nextRecord = csvReader.readNext()) != null) {
 	            for (String cell : nextRecord) {
 	                System.out.print(cell + "\t");
